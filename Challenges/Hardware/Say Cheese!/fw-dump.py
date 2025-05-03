e16f528a0530721e97ac299ea08375072c81d090
KOREONE42 <kore@kore.one>
2025-05-03 14:38:55 +0200

    new challenges added

---
+++ Challenges/Hardware/Say Cheese!/fw-dump.py
@@ -0,0 +1,45 @@
+import socket
+import json
+from pwn import * 
+
+def exchange(hex_list, value=0):
+    # Configure according to your setup
+    host = '83.136.250.201'  # The server's hostname or IP address
+    port = 58772        # The port used by the server
+    cs=0 # /CS on A*BUS3 (range: A*BUS3 to A*BUS7)
+    
+    usb_device_url = 'ftdi://ftdi:2232h/1'
+    # Convert hex list to strings and prepare the command data
+    command_data = {
+        "tool": "pyftdi",
+        "cs_pin":  cs,
+        "url":  usb_device_url,
+        "data_out": [hex(x) for x in hex_list],  # Convert hex numbers to hex strings
+        "readlen": value
+    }
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        
+        # Serialize data to JSON and send
+        s.sendall(json.dumps(command_data).encode('utf-8'))
+        
+        # Receive and process response
+        data = b''
+        while True:
+            data += s.recv(1024)
+            if data.endswith(b']'):
+                break
+                
+        response = json.loads(data.decode('utf-8'))
+        #print(f"Received: {response}")
+    return response
+
+# Dump du firmware
+f = open("dump.bin", "wb")
+for i in range(256):
+	print(i)
+	jedec = exchange([0x03,i,0,0], 65536)
+	for i in jedec:
+		f.write(p8(i))
+f.close()
